f9d30ba742523224501ced1c9f64575e9dd1cc43
naeemul-online <naeemul.online@gmail.com>
2025-07-13 17:52:00 +0300

    part-1 updated: route, trycatch repititive fix

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,10 +5,11 @@ dotenv.config();
 interface EnvConfig {
   PORT: string;
   DB_URL: string;
+  NODE_ENV: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-  const requiredEnvVariables: string[] = ["PORT", "DB_URL"];
+  const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
       throw new Error(`Missing requirement variable ${key}`);
@@ -18,6 +19,7 @@ const loadEnvVariables = (): EnvConfig => {
     PORT: process.env.PORT as string,
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     DB_URL: process.env.DB_URL!,
+    NODE_ENV: process.env.development as string,
   };
 };
 
---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { UserRoutes } from "../modules/user/user.route";
+
+export const router = Router();
+
+const moduleRoute = [
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+];
+
+moduleRoute.forEach((route) => {
+  router.use(route.path, route.route);
+});
